51652b6ba36f4b2c34f8d56779e52628db8c6501
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-29 22:45:43 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,18 +4,16 @@ const { PORT } = require('./config')
 
 const db = knex({
   client: 'pg',
-  ssl: { rejectUnauthorized: false },
-  //connection: DATABASE_URL,
-  //heroku connection
   connection: {
       host : 'ec2-18-233-83-165.compute-1.amazonaws.com',
       user: 'zkwnrvbuimcdel',
       database : 'd3qj30j6aiscv',
       password : '3ba026693655b972b960d98011aa6cf586edafd4478c834a229722864f4e4e80',
+      ssl: { 
+        rejectUnauthorized: false 
+      }
     }
 })
-
-  
 
 app.set('db', db)
 
